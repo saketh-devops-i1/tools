064c8f7c21a495b580166603254198841d8c4dfa
saketh <uday.kundoor123@gmail.com>
2024-04-17 17:12:02 +0530

    dns

--- module/main.tf
+++ module/main.tf
@@ -9,12 +9,21 @@ resource "aws_instance" "instance" {
 }
 
 resource "aws_route53_record" "record" {
-  name = "${var.tool_name}-internal"
-  type = "A"
+  name    = var.tool_name
+  type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.public_ip]
-  ttl = 30
+  ttl     = 30
 }
+
+resource "aws_route53_record" "record-internal" {
+  name    = "${var.tool_name}-internal"
+  type    = "A"
+  zone_id = var.zone_id
+  records = [aws_instance.instance.private_ip]
+  ttl     = 30
+}
+
 
 
 resource "aws_iam_role" "role" {
