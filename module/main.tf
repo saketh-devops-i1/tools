08b118baaeea27f0cd8a768ccc364f4d3098bee2
saketh <uday.kundoor123@gmail.com>
2024-04-17 18:46:29 +0530

    spot instance

--- module/main.tf
+++ module/main.tf
@@ -4,6 +4,14 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [ data.aws_security_group.selected.id ]
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
 
+instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
+  }
+
   tags = {
     Name = var.tool_name
   }
